adf28c1e81915c3463eef057fbeae1ffca083924
HenningSkar <henning.skar@gmail.com>
2019-10-19 22:06:48 +0200

    Triggers

---
+++ Seventh Father of the House/Assets/Scripts/Interaction/SceneTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTrigger : InteractionTrigger
+{
+
+    protected override void TriggerEnter(Collider coll)
+    {
+        
+    }
+
+    protected override void TriggerExit(Collider coll)
+    {
+        
+    }
+}
