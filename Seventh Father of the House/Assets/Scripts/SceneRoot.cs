fc3d29de0482d3590af338d6246521797518e14d
HenningSkar <henning.skar@gmail.com>
2019-10-20 15:46:30 +0200

    Translation of scene root

--- Seventh Father of the House/Assets/Scripts/SceneRoot.cs
+++ Seventh Father of the House/Assets/Scripts/SceneRoot.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
@@ -8,14 +9,36 @@ public class SceneRoot : MonoBehaviour
     [SerializeField]
     private AssetReference _sceneReference;
     [SerializeField]
+    private AssetReference _unloadReference;
+    [SerializeField]
     private int _yRotation;
     [SerializeField]
     private Transform _roomTransform;
 
+    public UnityEngine.ResourceManagement.ResourceProviders.SceneInstance sceneInstance;
 
+    [SerializeField]
+    private bool _translateOnEnable = true;
 
     private void OnEnable()
     {
-        _roomTransform.Rotate(0, _yRotation, 0);
+        if (!_translateOnEnable)
+        {
+            if (_roomTransform == null)
+                _roomTransform = this.transform;
+
+            _roomTransform.Rotate(0, _yRotation, 0);
+        }
+
+        GameManager.Instance.RoomLoadedCallback += OnRoomLoaded;
+    }
+
+    private void OnRoomLoaded(AssetReference obj)
+    {
+        if (obj.RuntimeKey.Equals(_unloadReference.RuntimeKey))
+        {
+            if(sceneInstance.Scene != null)
+                Addressables.UnloadSceneAsync(sceneInstance);
+        }
     }
 }
