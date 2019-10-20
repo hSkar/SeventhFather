54a6a5e923dceaa5377bc6c59e1e8ca66cef1fa2
HenningSkar <henning.skar@gmail.com>
2019-10-20 12:06:33 +0200

    SceneRoot added to all room scenes

---
+++ Seventh Father of the House/Assets/Scripts/SceneRoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SceneRoot : MonoBehaviour
+{
+    [SerializeField]
+    private AssetReference _sceneReference;
+    [SerializeField]
+    private int _yRotation;
+    [SerializeField]
+    private Transform _roomTransform;
+
+
+
+    private void OnEnable()
+    {
+        _roomTransform.Rotate(0, _yRotation, 0);
+    }
+}
