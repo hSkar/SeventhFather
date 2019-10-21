47bbe8be45e83f890d3713aaa1119781987fa717
HenningSkar <henning.skar@gmail.com>
2019-10-21 23:09:55 +0200

    janky sort of fix to animation syncing

--- Seventh Father of the House/Assets/QuillAnim/Scripts/QuillAnimComponent.cs
+++ Seventh Father of the House/Assets/QuillAnim/Scripts/QuillAnimComponent.cs
@@ -8,11 +8,18 @@ public class QuillAnimComponent : MonoBehaviour {
     public int frameRate = 12;
     public bool persistFrames = false;
         private bool _isPlaying = false;
+        public bool playOnStart;
+        public int startFrameDelay;
+
     private QuillAnimation _animation;
 
     void Start() {
         _animation = new QuillAnimation(this.transform, frameRate, persistFrames);
+            _animation._startFrameDelay = startFrameDelay;
         _animation.SetFrame(0);
+
+        if (playOnStart)
+            Play();
     }
 
    public void Play()
