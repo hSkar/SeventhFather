5a510464efc35104f2baba5e8ff0a4390f16b0ba
HenningSkar <henning.skar@gmail.com>
2019-10-22 14:05:22 +0200

    Picture frame and photos

--- Seventh Father of the House/Assets/Scripts/Managers/GameManager.cs
+++ Seventh Father of the House/Assets/Scripts/Managers/GameManager.cs
@@ -7,15 +7,28 @@ public class GameManager : MonoBehaviour
 {
     #region Singleton
     private static GameManager _instance;
+    private static bool _isQuitting;
 
+    [SerializeField]
+    private AssetReference _firstRoom;
+
+    
     public static GameManager Instance
     {
         get
         {
+            if (_isQuitting)
+                return null;
+
             if(_instance == null)
             {
-                var go = new GameObject("GameManager[SINGLETON]");
-                _instance = go.AddComponent<GameManager>();
+                _instance = FindObjectOfType<GameManager>();
+
+                if (_instance == null)
+                {
+                    var go = new GameObject("GameManager[SINGLETON]");
+                    _instance = go.AddComponent<GameManager>();
+                }
             }
 
             return _instance;
@@ -40,12 +53,27 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
-        PlayerCamera = Camera.main.transform;
+
+        if (!PlayerCamera)
+            PlayerCamera = Camera.main.transform;
+
+        LoadFirstRoom();
+
+    }
+
+    private void LoadFirstRoom()
+    {
+        Addressables.LoadSceneAsync(_firstRoom, UnityEngine.SceneManagement.LoadSceneMode.Additive);
     }
 
     public void OnLoadedRoom(AssetReference sceneRef)
     {
         LastLoadedRoom = sceneRef;
         RoomLoadedCallback?.Invoke(sceneRef);
+    }
+
+    private void OnApplicationQuit()
+    {
+        _isQuitting = true;
     }
 }
